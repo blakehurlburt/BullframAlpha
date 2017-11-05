78adc0c51723e5869d6d724c70e2931c20cc2658
Blake Hurlburt <blakehur@buffalo.edu>
2017-11-05 00:33:34 -0400

    Fix binary operators :-)

--- parser.py
+++ parser.py
@@ -9,7 +9,7 @@ def action(ctor):
 
 var = pp.Word(pp.alphas).setParseAction(action(AST.Var))
 number = pp.Word(pp.nums) + pp.Optional(pp.Literal(".") + pp.Word(pp.nums))
-number.setParseAction(action(lambda s: AST.Num(int(s))))
+number.setParseAction(action(lambda s: AST.Num(float(s))))
 
 plus  = pp.Literal("+")
 minus = pp.Literal("-")
@@ -43,58 +43,85 @@ func.setParseAction(__funcParseAction)
 
 operand = func | number | var
 
+class LeftBinOp:
+    def __init__(self, tokens):
+        self.operators = tokens[0][1::2]
+        self.operands = tokens[0][0::2]
+
+    def map(self, func):
+        res = LeftBinOp([[]])
+        res.operators = self.operators[:]
+        res.operands = [opr.map(func) for opr in self.operands]
+        return func(res)
+
 expr << pp.Or(pp.operatorPrecedence(operand, [
-    (minus, 1, pp.opAssoc.RIGHT),
-    (pwrop, 2, pp.opAssoc.RIGHT),
-    (mulop, 2, pp.opAssoc.LEFT),
-    (addop, 2, pp.opAssoc.LEFT),
+    (minus, 1, pp.opAssoc.RIGHT, lambda t: AST.Neg(t[0][1])),
+    (pwrop, 2, pp.opAssoc.RIGHT, lambda t: AST.Pow(t[0][0], t[0][2])),
+    (mulop, 2, pp.opAssoc.LEFT, LeftBinOp),
+    (addop, 2, pp.opAssoc.LEFT, LeftBinOp),
 ]), func)
-
-def __exprParseAction(fullString, index, arg):
-    if not isinstance(arg, pp.ParseResults):
-        # another action has already taken place
-        return arg
-
-    if len(arg) == 1:
-        arg = arg[0]
-        if isinstance(arg, AST.Expr):
-            # another action has already taken place
-            return arg
-
-    ops = {
-        "+": AST.Add,
-        "-": AST.Sub,
-        "*": AST.Mul,
-        "/": AST.Div,
-        "^": AST.Pow,
-    }
-
-    if len(arg) < 3: # unary
-        return AST.Neg(arg[1])
-
-    op = arg[1]
-
-    args = (__exprParseAction(fullString, index, arg[0]),
-            __exprParseAction(fullString, index, arg[2]))
-
-    if op in "+*":
-        return ops[op](args)
-    else:
-        return ops[op](*args)
-
-expr.setParseAction(__exprParseAction)
 
 pattern = expr + pp.StringEnd()
 
 def parse(string):
-    return pattern.parseString(string)[0]
+    res = expr.parseString(string)[0]
+    def convertLeftBinOp(obj):
+        if not isinstance(obj, LeftBinOp):
+            return obj
+
+        ops = {
+            '+': AST.Add,
+            '-': AST.Sub,
+            '*': AST.Mul,
+            '/': AST.Div
+        }
+        def makeNode(op, left, right):
+            if op in '+*':
+                return ops[op]([left, right])
+            else:
+                return ops[op](left, right)
+
+        root = makeNode(obj.operators[0], obj.operands[0], obj.operands[1])
+        cur = root
+
+        for rhs, operator in zip(obj.operands[1:], obj.operators[2:]):
+            if isinstance(cur, AST.Add):
+                lhs = cur.terms[-1]
+            elif isinstance(cur, AST.Sub):
+                lhs = cur.right
+            elif isinstance(cur, AST.Mul):
+                lhs = cur.factors[-1]
+            elif isinstance(cur, AST.Div):
+                lhs = cur.bottom
+
+            newNode = makeNode(operator, lhs, rhs)
+
+            if isinstance(cur, AST.Add):
+                cur.terms[-1] = newNode
+            elif isinstance(cur, AST.Sub):
+                cur.right = newNode
+            elif isinstance(cur, AST.Mul):
+                cur.factors[-1] = newNode
+            elif isinstance(cur, AST.Div):
+                cur.bottom = newNode
+
+            cur = newNode
+
+        return root
+
+    return res.map(convertLeftBinOp)
+
 
 if __name__ == "__main__":
-    test = "deriv(sin(x^2^x - cos(4+3*x)) / 4*(-2 +x), x)"
+    test = "deriv(sin(x^2^x - cos(4+3*x)) / 4*(-2 +x + 3 - 1), x)"
     #test = "int(sin(x^2^x - cos(4+3*x)) / 4*(-2 +x), x, -sin(x), sin(x))"
     #test = "sin / 2"
     #test = "sin(x^2^x - 3*x+4)"
     #test = "(x^2^x - 3*x + 4) / 2"
-    print(test)
+    #test = "1 * 2 + 3 - 4 / 5 + 6"
+    # test = "1 + 2 + 3 - 4 * 5 + 6"
+    #test = "1+2"
+    # test = "deriv(x + 2 * x + 3 + 4 + 5 * x, x)"
+    print("Input:", test)
     res = parse(test)
-    print(res)
+    print("Result:", res)
